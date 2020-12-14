cc48f5c49bb4a914d973ebef3b8ebfb8749f16d9
斎藤正大 <saima.ptw88@gmail.com>
2020-12-15 05:32:48 +0900

    devise追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -16,12 +16,9 @@
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.email }
+    password { "123456" }
+  end
 end
